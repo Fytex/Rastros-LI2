4324e7563ae6d8aa8c37aa1de4f0a56c71d45f8d
CJGNP <pierini10@gmail.com>
2020-05-02 22:11:01 +0100

    Changed flood fill function
    Added a condition in order to not play on the opponent's objetive and, when the objetive is not reachable, now does a arithmetic calculation to not get close of the opponent objetive.

--- logic/game.c
+++ logic/game.c
@@ -84,7 +84,7 @@ unsigned int game_finished(const State* const state) {
  * Gives a random position
  */
 
-Position RandomJog (State* state){
+Position randomJog (State* state){
     const int sum_pos[8][2] = {{1, 1}, {1, 0}, {1, -1}, {0, 1}, {0, -1}, {-1, 1}, {-1, 0}, {-1, -1}};
     const Position last_play=get_last_play(state);
     Position positions[8],pos;
@@ -125,8 +125,62 @@ Position RandomJog (State* state){
  */
 
 void computer_move2 (State* const state){
-    Position pos = RandomJog(state);
+    Position pos = randomJog(state);
     make_move(state,pos);
+}
+
+/*
+ * Choses the adjacent position that is near from the objetive
+ */
+Position arithmeticJog (State* state, unsigned int player){
+    int d, d1;
+    Position* jogpos;
+    Position* jogpos1;
+    const int sum_pos[8][2] = {{1, 1}, {1, 0}, {1, -1}, {0, 1}, {0, -1}, {-1, 1}, {-1, 0}, {-1, -1}};
+    const Position last_play=get_last_play(state);
+    Position positions[8],pos;
+    int length = 0;
+
+    List* position_list = NULL;
+
+
+    position_list = create_list();
+
+    for (int i=0; i < 8; ++i) {
+        pos = (Position) {.row = last_play.row + sum_pos[i][0], .column = last_play.column + sum_pos[i][1]};
+
+
+        if (pos.row >= 0 && pos.row < 8 && pos.column >= 0 && pos.column < 8 &&
+            get_position_space(state, pos) == Blank) {
+
+            positions[length] = pos;
+            position_list = head_insert(position_list, positions + length);
+            length++;
+        }
+
+    }
+
+    jogpos = get_head(position_list);
+
+    d = ((7 * ((int) player - 1)) - jogpos->column) * ((7 * ((int) player - 1)) - jogpos->column)
+        + (7 - (7 * ((int) player - 1)) - jogpos->row) * (7 - (7 * ((int) player - 1)) - jogpos->row);
+
+    position_list = remove_head(position_list);
+
+    while (position_list->next != NULL) {
+        jogpos1 = get_head(position_list);
+        d1 = ((7 * ((int) player - 1)) - jogpos1->column) * ((7 * ((int) player - 1)) - jogpos1->column)
+            + (7 - (7 * ((int) player - 1)) - jogpos1->row) * (7 - (7 * ((int) player - 1)) - jogpos1->row);
+
+        if (d1 < d) {
+            d = d1;
+            jogpos = jogpos1;
+        }
+        position_list = remove_head(position_list);
+    }
+
+    return *jogpos;
+
 }
 
 
@@ -186,7 +240,10 @@ Position flood_fill(State* state, int flood_fill_matrix[][8] ,unsigned int playe
                     return reads_flood_fill_matrix(flood_fill_matrix, pos, count);
                 }
 
-                if (pos.row >= 0 && pos.row < 8 && pos.column >= 0 && pos.column < 8 &&
+                if ((unsigned) pos.row == (7 * (player - 1)) && (unsigned) pos.column == 7 - (7 * (player - 1)))
+                    flood_fill_matrix[pos.row][pos.column] = -1;
+
+                else if (pos.row >= 0 && pos.row < 8 && pos.column >= 0 && pos.column < 8 &&
                     flood_fill_matrix[pos.row][pos.column] == 0) {
 
                     positions[length] = pos;
@@ -203,7 +260,7 @@ Position flood_fill(State* state, int flood_fill_matrix[][8] ,unsigned int playe
         position_list2 = clear_list();
         count++;
     }
-    return RandomJog(state); //in case of being impossible to reach the objetive, does a random play
+    return arithmeticJog(state, player); //in case of being impossible to reach the objetive, does an arithmetic calculation
 }
 
 
